b726e73db36fd11fcaefec842d5fe7e906864c29
bevislee1024 <bevislee1024@gmail.com>
2019-03-01 11:19:45 +0800

    update Program.cs

--- gittest/Program.cs
+++ gittest/Program.cs
@@ -16,6 +16,7 @@ namespace gittest
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+			string a = "test";
         }
     }
 }
